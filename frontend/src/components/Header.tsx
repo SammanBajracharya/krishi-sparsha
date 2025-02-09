4f8ad3ea1d2f673a1a47c0eeec2b2f4847967376
Pushkar Niraula <puskarniraula24@gmail.com>
2025-02-09 16:56:46 +0545

    ui changes

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button"
+import logo from '../assets/krishi.svg'
+
+function Header() {
+  return (
+    <div className="flex justify-between items-center p-4 border-b-[1px] border-gray-500">
+        <div><img src={logo} alt="Logo" /></div>
+        <div className="flex">
+            <nav className="w-full flex gap-8 justify-between text-lg">
+                <ul><a href="#">Home</a></ul>
+                <ul><a href="#">Marketplace</a></ul>
+                <ul><a href="#">Find Deals</a></ul>
+            </nav>
+        </div>
+        <div className="flex gap-4">
+        <Button variant="outline">Log in</Button>
+        <Button variant="secondary">Sign up</Button>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
