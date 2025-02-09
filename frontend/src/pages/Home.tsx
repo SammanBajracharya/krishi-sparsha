77dc9c85f4c04d83f1e0651be9ca83baf9e8daff
anjolina <anjolinapradhan296@gmail.com>
2025-02-09 17:33:42 +0545

    homepage

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <><h6>krishi-स्पर्श seeks to find new way farmer can sustain their business</h6>
+    <p>We are a go-to online marketplace designed exclusively for framers, artisans, and businesses in the framing industry.</p>
+    <button>Explore</button>
+    <img src="" alt="image of a land" />
+    <div>
+      <p>How we work</p>
+      <h6>krishi-स्पर्श is not just an e-commerce site, it trust bond between farmers and consumer</h6>
+    </div>
+    </>
+
+  );
+};
+
+export default Home;
