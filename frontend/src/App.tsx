e100798724c2eea0f85c95c0285a7aff59d3f236
Samman Bajracharya <sammanbajracharya@gmail.com>
2025-02-09 17:20:57 +0545

    react-route-dom added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import FindDealsPage from "./pages/FindDeals";
+import Header from "./components/Header";
 
 function App() {
-
-
-  return (
-    <>
-      <Header/>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Home />
+                    }
+                />
+                <Route
+                    path="/find-deals"
+                    element={ <FindDealsPage /> }
+                />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ frontend/src/pages/FindDeals.tsx
@@ -0,0 +1,12 @@
+import { Card, CardHeader } from "../components/ui/card";
+
+const FindDealsPage = () => {
+    return (
+        <Card>
+            <CardHeader>
+            </CardHeader>
+        </Card>
+    );
+}
+
+export default FindDealsPage;
